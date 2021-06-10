e2ce2c4575b417cdf3a286a316e81792f0219273
Felix <felix-mayer@gmx.de>
2021-06-10 16:41:54 +0200

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import Main from './views/Main';
+import React from 'react';
+import { Router } from '@reach/router';
+import Detail from './views/Detail';
+import Update from './views/Update';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Main path="products/"/>
+        <Detail path="products/:id" />
+        <Update path="products/:id/edit" />
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
